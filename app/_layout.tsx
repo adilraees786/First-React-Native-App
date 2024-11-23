e40c0d562863e8228c5c78be9b4e7b5b98e29735
adilraees786 <adilraees911@gmail.com>
2024-11-23 12:46:23 +0500

    add hello

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,9 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { View } from 'react-native-reanimated/lib/typescript/Animated';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,12 +30,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <view>
+      <text>hello</text>
+      
+      
+    </view>
   );
 }
