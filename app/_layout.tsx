3cad41d8124903e7c2e204227091793e649517f4
adilraees786 <adilraees911@gmail.com>
2024-11-29 16:21:26 +0500

    add deliveru icom

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -49,7 +49,7 @@ export default function Bykea() {
         <Card title={"Ride"} icon={"bike-scooter"} bgcolor={"#bedadc"}/>
         </View>
         <View style={styles.row}>
-        <Card title={"Carpol"} icon={"electric-car"} bgcolor={"#c1e1c5"}/>
+        <Card title={"Carpol"} icon={"delivery-dining"} bgcolor={"#c1e1c5"}/>
         <Card title={"Ride"} icon={"bike-scooter"} bgcolor={"#bedadc"}/>
         </View>
         <View style={styles.row}>
